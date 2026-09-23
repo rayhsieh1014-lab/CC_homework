1edf91a8b75d0a0d8bc18904baef318580c9f707
rayhsieh1014-lab <rayhsieh1014@gmail.com>
2026-09-23 21:43:54 +0800

    Add files via upload

---
+++ 2.25.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("H     H\n");
+    printf("H     H\n");
+    printf("H     H\n");
+    printf("HHHHHHH\n");
+    printf("H     H\n");
+    printf("H     H\n");
+    printf("H     H\n");
+
+    printf("\n");
+
+    printf("ZZZZZZZ\n");
+    printf("     Z \n");
+    printf("    Z  \n");
+    printf("   Z   \n");
+    printf("  Z    \n");
+    printf(" Z     \n");
+    printf("ZZZZZZZ\n");
+
+    printf("\n");
+
+    printf("RRRRRR \n");
+    printf("R     R\n");
+    printf("R     R\n");
+    printf("RRRRRR \n");
+    printf("R   R  \n");
+    printf("R    R \n");
+    printf("R     R\n");
+
+    return 0;
+}
